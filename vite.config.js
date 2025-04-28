1993fe419a1400d7b647f7367a1291942e27bde6
gumnaam23 <aqibdev23@gmail.com>
2025-04-28 22:24:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target: "http://localhost:8000"
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: true,
+        },
       },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  }
 })
